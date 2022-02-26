b26bb6fb2ffe766ca45ff442565635cb7c8e5c24
Jrsf22 <2001034D@student.tp.edu.sg>
2022-02-27 00:01:53 +0800

    asd

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,49 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	 //declare Selenium WebDriver
+	  private WebDriver webDriver;		
+	  
+	  @Test
+	  public void checkId() {
+		  //Load website as a new page
+		  webDriver.navigate().to("http://localhost:8090/GroupProject/BookServlet1/dashboard");
+		  WebElement we =  webDriver.findElement(By.id("content"));
+		  
+		  System.out.println("id we: "+we.getAttribute("class"));
+		  Assert.assertEquals(we.getAttribute("class"), "row");
+	  }
+	  @Test
+	  public void checkTitle() {
+		  //Load website as a new page
+		  webDriver.navigate().to("http://localhost:8090/GroupProject/BookServlet1/dashboard");
+		  
+		  //Assert the title to check that we are indeed in the correct website
+		  Assert.assertEquals(webDriver.getTitle(), "Book Management");
+		  
+		  System.out.println("title: "+webDriver.getTitle());
+		  
+	  }
+	  
+	  
+	  
+	  @BeforeTest
+	  public void beforeTest() {
+		  //Setting system properties of ChromeDriver
+		  //to amend directory path base on your local file path
+		  String chromeDriverDir = "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe";
+
+		  System.setProperty("webdriver.chrome.driver", chromeDriverDir);
+
+		  //initialize FirefoxDriver at the start of test
+		  webDriver = new ChromeDriver();  
+	  }
+
+	  @AfterTest
+	  public void afterTest() {
+		  //Quit the ChromeDriver and close all associated window at the end of test
+//		  webDriver.quit();			
+	  }
+}
